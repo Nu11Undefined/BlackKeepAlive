606fb55c76a3dd47f47958264aef8fa0951bb536
Nu11Undefined <45036532+Nu11Undefined@users.noreply.github.com>
2026-03-22 22:10:33 +0300

    Update MainActivity.java

--- app/src/main/java/com/min/black/MainActivity.java
+++ app/src/main/java/com/min/black/MainActivity.java
@@ -1,17 +1,26 @@
 package com.min.black;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.view.View;
 import android.graphics.Color;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Держим экран включенным
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        View view = new View(this);
-        view.setBackgroundColor(Color.BLACK);
-        setContentView(view);
+        
+        // Устанавливаем черный фон
+        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+        
+        // Скрываем статус-бар и навигацию (полноэкранный режим)
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
     }
 }
